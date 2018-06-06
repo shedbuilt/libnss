15c8d60cf6a8fad126e5da785dd1ede784c9bebd
Auston Stewart <auston@shedbuilt.net>
2018-06-06 09:08:57 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-patch -Np1 -i "${SHED_PATCHDIR}/nss-3.36.1-standalone-1.patch" &&
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/nss-3.36.1-standalone-1.patch" &&
 cd nss &&
 make -j1 BUILD_OPT=1 \
          NSPR_INCLUDE_DIR=/usr/include/nspr \
@@ -9,13 +9,13 @@ make -j1 BUILD_OPT=1 \
          NSS_USE_SYSTEM_SQLITE=1 \
          $([[ $SHED_NATIVE_TARGET =~ ^aarch64-.* ]] && echo USE_64=1) &&
 cd ../dist &&
-install -v -m755 -d "${SHED_FAKEROOT}/usr/lib" &&
-install -v -m755 Linux*/lib/*.so "${SHED_FAKEROOT}/usr/lib" &&
-install -v -m644 Linux*/lib/{*.chk,libcrmf.a} "${SHED_FAKEROOT}/usr/lib" &&
-install -v -m755 -d "${SHED_FAKEROOT}/usr/include/nss" &&
-cp -v -RL {public,private}/nss/* "${SHED_FAKEROOT}/usr/include/nss" &&
-chmod -v 644 "${SHED_FAKEROOT}"/usr/include/nss/* &&
-install -v -m755 -d "${SHED_FAKEROOT}/usr/bin" &&
-install -v -m755 Linux*/bin/{certutil,nss-config,pk12util} "${SHED_FAKEROOT}/usr/bin" &&
-install -v -m755 -d "${SHED_FAKEROOT}/usr/lib/pkgconfig" &&
-install -v -m644 Linux*/lib/pkgconfig/nss.pc "${SHED_FAKEROOT}/usr/lib/pkgconfig"
+install -v -m755 -d "${SHED_FAKE_ROOT}/usr/lib" &&
+install -v -m755 Linux*/lib/*.so "${SHED_FAKE_ROOT}/usr/lib" &&
+install -v -m644 Linux*/lib/{*.chk,libcrmf.a} "${SHED_FAKE_ROOT}/usr/lib" &&
+install -v -m755 -d "${SHED_FAKE_ROOT}/usr/include/nss" &&
+cp -v -RL {public,private}/nss/* "${SHED_FAKE_ROOT}/usr/include/nss" &&
+chmod -v 644 "${SHED_FAKE_ROOT}"/usr/include/nss/* &&
+install -v -m755 -d "${SHED_FAKE_ROOT}/usr/bin" &&
+install -v -m755 Linux*/bin/{certutil,nss-config,pk12util} "${SHED_FAKE_ROOT}/usr/bin" &&
+install -v -m755 -d "${SHED_FAKE_ROOT}/usr/lib/pkgconfig" &&
+install -v -m644 Linux*/lib/pkgconfig/nss.pc "${SHED_FAKE_ROOT}/usr/lib/pkgconfig"
